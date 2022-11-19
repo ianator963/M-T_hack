d250ffd96674720289002439e4b0283a8521726c
Rachel <ral4123@g.rit.edu>
2022-11-19 13:07:48 -0500

    starting from scratch

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-@app.route("/login")
-def login():
-    return "<h1>Welcome<h1>"
-
-
-if __name__ == "__main__":
-    app.run(debug=True) # listening on port 5000
+if __name__ == '__main__': 
+   app.run(port=5000, debug=True)
--- datetime.py
+++
@@ -1,4 +0,0 @@
-import time
-
-year, mon, mday, hour, min, sec, wday, yday, isdst = time.localtime()
-print(f' year: {year} mon: {mon} mday: {mday} hour: {hour} min: {min} sec: {sec} wday: {wday} yday: {yday} isdst: {isdst}')
