63b43f2be39e2ddb1393d697499bc4f1b1a6c830
Rachel <ral4123@g.rit.edu>
2022-11-19 13:00:29 -0500

    attempting to make an html file

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello World"
+def home():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return "<h1>Welcome<h1>"
+
 
 if __name__ == "__main__":
-    app.run(port="5000", debug=True) # listening on port 5000
+    app.run(debug=True) # listening on port 5000
