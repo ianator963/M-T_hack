67a26de76c9e59afa5e0bbbb0b7ef1f036f3f804
Kasey Kiggins <kak8381@.rit.edu>
2022-11-20 08:43:29 -0500

    printing calendar

--- calendar_main.py
+++ calendar_main.py
@@ -4,37 +4,38 @@ import calendar_code
 """
 in this module take the calendar module and import dates into it
 """ 
-Nov = MplCalendar(2022, 10)
-# Nov.add_event()
-Nov.show()
-import re 
-def add_event(filename, Calendar):
-    a_list=[]
-    with open(filename) as file:
-        next(file)
-        for line in file: 
-            date = line.strip('').split("/")
-            print(date, len(date))
-            for index in date:
-                a_list.append(index)
-    # for letter in a_list:
-    #     if letter is not str(letter).isalpha():
-    #         print(letter)
-    while len(a_list)>0:
-        i=0
-        day = a_list[i+1]
-        event= a_list[i]
-        res = re.sub(r'[^a-zA-Z]', '', event)
-        a_list.pop(i+2)
-        a_list.pop(i+1)
-        a_list.pop(i)
-        Calendar.add_event(day, res)
+# import re 
+# def add_event(filename, Calendar):
+#     a_list=[]
+#     with open(filename) as file:
+#         next(file)
+#         for line in file: 
+#             date = line.strip('').split("/")
+#             for index in date:
+#                 a_list.append(index)
+#     # for letter in a_list:
+#     #     if letter is not str(letter).isalpha():
+#     #         print(letter)
+#     while len(a_list)>0:
+#         i=0
+#         day = a_list[i+1]
+#         event= a_list[i]
+#         res = re.sub(r'[^a-zA-Z]', '', event)
+#         a_list.pop(i+2)
+#         a_list.pop(i+1)
+#         a_list.pop(i)
+#         return Calendar.add_event(int(day), res)
 
 
-def main():
-    filename = "tasks.txt"
-    Nov = MplCalendar(2022, 10)
-    add_event(filename, Nov)
-    Nov.show()
+# def main():
+#     filename = "tasks.txt"
+#     Nov = MplCalendar(2022, 10)
+#     print(add_event(filename, Nov))
+#     print(Nov.show())
 
-main()
+# main()
+
+import calendar
+year = 2022
+month = 11
+print(calendar.month(year, month))
